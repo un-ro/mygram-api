5aff1096b792105962eb90d2fc772c910419cd13
Unero <ramadhanbhg@gmail.com>
2023-04-20 08:17:23 +0700

    fix: endpoints

--- routers/appRouter.go
+++ routers/appRouter.go
@@ -9,6 +9,12 @@ import (
 func StartServer() *gin.Engine {
 	r := gin.Default()
 
+	r.GET("/ping", func(context *gin.Context) {
+		context.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
 	userRouter := r.Group("/user")
 	{
 		userRouter.POST("/register", controllers.Register)
@@ -16,34 +22,34 @@ func StartServer() *gin.Engine {
 	}
 
 	r.Static("/img", "./assets")
-	photoRouter := r.Group("/photo")
+	photoRouter := r.Group("/photos")
 	{
 		photoRouter.Use(middlewares.Auth())
-		photoRouter.POST("/post", controllers.CreatePhoto)
-		photoRouter.GET("/getAll", controllers.GetAllPhotos)
-		photoRouter.GET("/getOne/:photoID", controllers.GetPhoto)
-		photoRouter.PUT("/update/:photoID", middlewares.PhotoAuth(), controllers.UpdatePhoto)
-		photoRouter.DELETE("/delete/:photoID", middlewares.PhotoAuth(), controllers.DeletePhoto)
+		photoRouter.POST("", controllers.CreatePhoto)
+		photoRouter.GET("", controllers.GetAllPhotos)
+		photoRouter.GET("/:photoID", controllers.GetPhoto)
+		photoRouter.PUT("/:photoID", middlewares.PhotoAuth(), controllers.UpdatePhoto)
+		photoRouter.DELETE("/:photoID", middlewares.PhotoAuth(), controllers.DeletePhoto)
 	}
 
-	commentRouter := r.Group("/comment")
+	commentRouter := r.Group("/comments")
 	{
 		commentRouter.Use(middlewares.Auth())
-		commentRouter.POST("/create", controllers.CreateComment)
-		commentRouter.GET("/getAll", controllers.GetAllComment)
-		commentRouter.GET("/getOne/:commentID", controllers.GetOneComment)
-		commentRouter.PUT("/update/:commentID", middlewares.CommentAuth(), controllers.UpdateComment)
-		commentRouter.DELETE("/delete/:commentID", middlewares.CommentAuth(), controllers.DeleteComment)
+		commentRouter.POST("", controllers.CreateComment)
+		commentRouter.GET("", controllers.GetAllComment)
+		commentRouter.GET("/:commentID", controllers.GetOneComment)
+		commentRouter.PUT("/:commentID", middlewares.CommentAuth(), controllers.UpdateComment)
+		commentRouter.DELETE("/:commentID", middlewares.CommentAuth(), controllers.DeleteComment)
 	}
 
 	socialMediaRouter := r.Group("/social-media")
 	{
 		socialMediaRouter.Use(middlewares.Auth())
-		socialMediaRouter.POST("/create", controllers.CreateSocialMedia)
-		socialMediaRouter.GET("/getAll", controllers.GetAllSocialMedia)
-		socialMediaRouter.GET("/getOne/:socialMediaID", controllers.GetOneSocialMedia)
-		socialMediaRouter.PUT("/update/:socialMediaID", middlewares.SocialMediaAuth(), controllers.UpdateSocialMedia)
-		socialMediaRouter.DELETE("/delete/:socialMediaID", middlewares.SocialMediaAuth(), controllers.DeleteSocialMedia)
+		socialMediaRouter.POST("", controllers.CreateSocialMedia)
+		socialMediaRouter.GET("", controllers.GetAllSocialMedia)
+		socialMediaRouter.GET("/:socialMediaID", controllers.GetOneSocialMedia)
+		socialMediaRouter.PUT("/:socialMediaID", middlewares.SocialMediaAuth(), controllers.UpdateSocialMedia)
+		socialMediaRouter.DELETE("/:socialMediaID", middlewares.SocialMediaAuth(), controllers.DeleteSocialMedia)
 	}
 
 	return r
