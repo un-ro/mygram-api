7889038b3bac763ddfd8b83cafc918cfc4cf71ba
Unero <ramadhanbhg@gmail.com>
2023-04-20 08:18:04 +0700

    implement .env

--- database/db.go
+++ database/db.go
@@ -3,18 +3,12 @@ package database
 import (
 	"MyGram/models"
 	"fmt"
+	"github.com/joho/godotenv"
 	"log"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-)
-
-const (
-	HOST     = "localhost"
-	PORT     = "32768"
-	USERNAME = "postgres"
-	PASSWORD = "postgrespw"
-	DBNAME   = "fga"
 )
 
 var (
@@ -22,8 +16,23 @@ var (
 	err error
 )
 
+func initDB() string {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalln("Error loading .env file")
+	}
+
+	HOST := os.Getenv("DB_HOST")
+	PORT := os.Getenv("DB_PORT")
+	USERNAME := os.Getenv("DB_USERNAME")
+	PASSWORD := os.Getenv("DB_PASSWORD")
+	DBNAME := os.Getenv("DB_NAME")
+
+	return fmt.Sprintf("host=%s user=%s password=%s port=%s dbname=%s sslmode=disable", HOST, USERNAME, PASSWORD, PORT, DBNAME)
+}
+
 func StartDB() {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s port=%s dbname=%s sslmode=disable", HOST, USERNAME, PASSWORD, PORT, DBNAME)
+	dsn := initDB()
 
 	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
@@ -32,7 +41,12 @@ func StartDB() {
 
 	log.Println("Success connect to database")
 
-	err := db.Debug().AutoMigrate(models.User{}, models.Photo{}, models.Comment{}, models.SocialMedia{})
+	err := db.Migrator().DropTable(models.User{}, models.Photo{}, models.Comment{}, models.SocialMedia{})
+	if err != nil {
+		log.Fatalln("Error drop table")
+	}
+
+	err = db.Debug().AutoMigrate(models.User{}, models.Photo{}, models.Comment{}, models.SocialMedia{})
 	if err != nil {
 		log.Fatalln("Error migrate database")
 	}
